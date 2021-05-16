f33a4b6bcbb350740a1aae9ec2277064ca1b77f4
Esrat <imranzaman.318@gmail.com>
2021-05-16 16:12:01 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Toggle from "./Toggle";
-
+import NumberList from "./NumberList";
 function App() {
   return (
     <div>
       <h3>Toggle</h3>
       <Toggle />
+      <hr />
+
+      <h3>NumberList</h3>
+      <NumberList />
+      <hr />
     </div>
   );
 }
